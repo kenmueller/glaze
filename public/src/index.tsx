a17105178edcf079ee4353307bef21f9bd3a292d
kenmueller <kenmueller0@gmail.com>
2020-05-14 16:14:05 -0700

    removed snap

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -6,11 +6,6 @@ import * as serviceWorker from './serviceWorker'
 
 import './scss/index.scss'
 
-const rootElement = document.getElementById('root') ?? document.body
-
-ReactDOM[rootElement.hasChildNodes() ? 'hydrate' : 'render'](
-	<App />,
-	rootElement
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.register()
